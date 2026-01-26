91fc7c40ea47a230ef9adbda7e8b31cfbed7f91d
Tajudeen Adamu <zakariyahabdulaleem10@gmail.com>
2026-01-26 19:51:51 +0000

    chore: add projects/market.xyz/index.js

---
+++ projects/market.xyz/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  deadFrom: '2023-10-22',
+  misrepresentedTokens: true,
+  polygon: {
+    tvl: () => ({}),
+    borrowed: () => ({}),
+  },
+  fantom: {
+    tvl: () => ({}),
+    borrowed: () => ({}),
+  },
+};
