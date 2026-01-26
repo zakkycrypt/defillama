a24c9a69a99795d770eba70e3b46d53a64625155
Tajudeen Adamu <zakariyahabdulaleem10@gmail.com>
2026-01-26 19:50:01 +0000

    chore: add projects/hydradex.js

---
+++ projects/hydradex.js
@@ -0,0 +1,8 @@
+module.exports = {
+  deadFrom: '2025-07-01',
+  misrepresentedTokens: true,
+  methodology: "We count liquidity on the dex, pulling data from subgraph",
+  hydra: {
+    tvl: () => ({}),
+  },
+};
