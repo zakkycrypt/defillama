d3afd08a73c4d4c7377c4acf810c9d2feb9a0e2a
Tajudeen Adamu <zakariyahabdulaleem10@gmail.com>
2026-01-28 15:53:15 +0000

    chore: add projects/mux/index.js

---
+++ projects/mux/index.js
@@ -0,0 +1,58 @@
+const abi = require("./abi.json");
+const mux3CoreAbi = require("./mux3CoreAbi.json");
+const { sumTokens2 } = require('../helper/unwrapLPs')
+
+const readerContract = {
+  arbitrum: '0x437CEa956B415e97517020490205c07f4a845168',
+  bsc: '0x2981Bb8F9c7f7C5b9d8CA5e41C0D9cBbd89C7489',
+  avax: '0xB33e3dDcE77b7679fA92AF77863Ae439C44c8519',
+  fantom: '0xfb0DCDC30BF892Ec981255e7133AEcb8ea642b76',
+  optimism: '0x572E9467b2585c3Ab6D9CbEEED9619Fd168254D5',
+}
+
+const mux3CoreAddress = '0x85c8F4a67F4f9AD7b38e875c8FeDE7F4c878bFAc'
+
+async function getMux3Tvl(api) {
+  // get all mux3 collateral pools
+  const pools = await api.call({
+    target: mux3CoreAddress,
+    abi: mux3CoreAbi.listCollateralPool,
+  })
+
+  // get all supported collateral tokens
+  const collateralTokens = await api.call({
+    target: mux3CoreAddress,
+    abi: mux3CoreAbi.listCollateralTokens,
+  })
+
+  // get balances of all collateral tokens in all collateral pools
+  return sumTokens2({ api, tokens: collateralTokens, owners: pools, })
+}
+
+async function tvl(api) {
+  const storage = await api.call({ target: readerContract[api.chain], abi: abi.getChainStorage, })
+  const pool = await api.call({ target: readerContract[api.chain], abi: abi.pool, })
+
+  const assets = storage[1]
+  const dexs = storage[2]
+  await sumTokens2({ api, tokens: assets.map(i => i.tokenAddress), owner: pool, })
+
+
+  dexs.forEach(dex => {
+    dex.liquidityBalance.forEach((balance, index) => {
+      const assetId = dex.assetIds[index]
+      const token = assets.find(t => assetId === t.id)
+      api.add(token.tokenAddress, balance)
+    })
+  })
+
+  // get mux3 tvl, only for arbitrum
+  if (api.chain === 'arbitrum')
+    await getMux3Tvl(api)
+}
+
+Object.keys(readerContract).forEach(chain => {
+  module.exports[chain] = {
+    tvl
+  }
+})
