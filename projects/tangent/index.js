cef91e3a9b69581595108f54ca131f9d8c110b7d
Tajudeen Adamu <zakariyahabdulaleem10@gmail.com>
2026-01-29 07:23:29 +0000

    chore: add projects/tangent/index.js

---
+++ projects/tangent/index.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+  deadFrom: '2023-07-09',
+  methodology: "Data is retrieved from the api at https://api.tangent.bar/",
+  timetravel: false,
+  hedera: { tvl: () => ({ }) }
+}
+
+
